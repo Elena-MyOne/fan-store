1ec9a50fb67f595fd87dff7a6e9f79a0533b3522
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-05-08 15:15:03 -0400

    feat: add footer layout

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="h-[100px]">
-      <div>Footer</div>
+    <footer className="h-[60px] bg-gray-100 flex justify-center items-center mx-3">
+      <div>
+        &copy; Developed by{' '}
+        <a
+          href="https://github.com/Elena-MyOne"
+          target="_blank"
+          rel="noreferrer"
+          className="pointer text-orange-500 hover:text-orange-600 duration-300"
+        >
+          Elena-myone
+        </a>
+      </div>
     </footer>
   );
 };
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,9 +5,9 @@ import Footer from '../Footer/Footer';
 
 const Layout = () => {
   return (
-    <div className="grid grid-rows-[80px_auto_50px]">
+    <div className="grid grid-rows-[80px_auto_60px]">
       <Header />
-      <main className="min-h-[86vh]">
+      <main className="min-h-[87vh]">
         <Outlet />
       </main>
       <Footer />
