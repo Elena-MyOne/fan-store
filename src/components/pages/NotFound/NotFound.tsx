7f42f760665aeabff82d5b24554f39fb2d7e64b6
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-05-08 15:39:20 -0400

    feat: add NotFound page  layout

--- src/components/pages/NotFound/NotFound.tsx
+++ src/components/pages/NotFound/NotFound.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { ROUTER_PATH } from '../../../models/enums';
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>404</h2>
-      <p>Page Not Found</p>
-    </div>
+    <section className="error h-full flex justify-center items-center flex-col gap-5">
+      <h2 className="title text-4xl">404</h2>
+      <p className="text-gray-600">Something goes wrong. Page Not Found</p>
+      <NavLink
+        to={ROUTER_PATH.HOME}
+        className="px-6 py-2 block text-white bg-gray-800 hover:bg-gray-900 duration-300 rounded-3xl"
+      >
+        Home page
+      </NavLink>
+    </section>
   );
 };
 
