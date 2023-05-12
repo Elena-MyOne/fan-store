9539c42968a449afe390a2d4f4935b708cd78422
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-05-12 16:24:30 -0400

    feat: add toggle active styles for categories in Categories component

--- src/components/pages/Home/Categories/Categories.tsx
+++ src/components/pages/Home/Categories/Categories.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProductsData } from '../../../../models/interface';
 
 interface ProductsDataProps {
@@ -6,24 +6,27 @@ interface ProductsDataProps {
 }
 
 const Categories = (props: ProductsDataProps) => {
+  const [activeCategory, setActiveCategory] = useState('all');
+
   const styleCategoryItem =
     'px-4 py-1 bg-gray-200 hover:bg-gray-200 duration-300 rounded-3xl cursor-pointer';
   const styleCategoryItemActive =
     'px-4 py-1 text-white bg-gray-800 hover:bg-gray-900 duration-300 rounded-3xl cursor-pointer';
 
   const allCategories = props.products.map((product) => product.category.toLowerCase());
-  const categories = [...new Set(allCategories)].map((category, index) => (
-    <li className={styleCategoryItem} key={index}>
+  const categories = ['all', ...new Set(allCategories)].map((category, index) => (
+    <li
+      className={category === activeCategory ? styleCategoryItemActive : styleCategoryItem}
+      key={index}
+      onClick={() => setActiveCategory(category ? category : 'all')}
+    >
       {category}
     </li>
   ));
 
   return (
     <div className="categories">
-      <ul className="category flex justify-between items-center gap-6">
-        <li className={styleCategoryItemActive}>all</li>
-        {categories}
-      </ul>
+      <ul className="category flex justify-between items-center gap-6">{categories}</ul>
     </div>
   );
 };
