78ea5ccac928b6cf11df33eafc2f9bc67eaeb24f
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-05-31 15:46:26 -0400

    fix: move axios requests into components/Services/Service.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Footer/Footer';
+import Service from '../Services/Service';
 
 const Layout = () => {
   return (
     <div className="grid grid-rows-[80px_auto_60px]">
+      <Service />
       <Header />
       <main className="min-h-[87vh]">
         <Outlet />
