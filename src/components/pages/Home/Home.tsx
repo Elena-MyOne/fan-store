0b58fca01abba8e1261d311720569108de3a432a
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-06-01 16:57:34 -0400

    feat: add change title on home page if products are sorted

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -3,8 +3,17 @@ import Categories from './Categories/Categories';
 import Products from './Products/Products';
 import Sort from './Sort/Sort';
 import Pagination from './Pagination/Pagination';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../../redux/store';
 
 const Home = () => {
+  const { activeCategory, activeFaculty } = useSelector((state: RootState) => state.filter);
+
+  const title =
+    activeCategory === 'all' && activeFaculty === 'All' ? 'All products' : 'Sort products';
+
+  // const title = setTitle();
+
   return (
     <div className="home container px-2 m-auto">
       <div className="top flex justify-between items-center gap-4 py-8 xl:flex-row flex-col">
@@ -13,7 +22,7 @@ const Home = () => {
       </div>
       <div className="products mb-4">
         <Pagination />
-        <h2 className="title font-semibold text-xl md:mb-4 mb-2">All products</h2>
+        <h2 className="title font-semibold text-xl md:mb-4 mb-2">{title}</h2>
         <Products />
       </div>
     </div>
