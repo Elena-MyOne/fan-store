5db424683e28277180b19d71d14cb33258068292
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-08-22 15:39:57 -0400

    feat: add product cards rendering for favorite component

--- src/components/pages/Favorite/Favorite.tsx
+++ src/components/pages/Favorite/Favorite.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import EmptyFavorite from './EmptyFavorite/EmptyFavorite';
 import FavoriteItems from './FavoriteItems/FavoriteItems';
+import { selectFavorite } from '../../../redux/slices/FavoriteSlice';
+import { useSelector } from 'react-redux';
 
 const Favorite: React.FC = () => {
-  const [isFavoriteItems, setisFavoriteItems] = React.useState(false);
+  const { isEmptyFavorite } = useSelector(selectFavorite);
 
   return (
     <section className="favorite container m-auto py-4 md:py-10 h-full px-2">
-      <div className="body h-full">{isFavoriteItems ? <FavoriteItems /> : <EmptyFavorite />}</div>
+      <div className="body h-full">{isEmptyFavorite ? <EmptyFavorite /> : <FavoriteItems />}</div>
     </section>
   );
 };
